df0dee9f3df778cb492bfbfb0e18436fd6c82438
tsmith-stampede <timsmith204@utexas.edu>
2017-04-15 20:03:55 -0500

    Compute graph adjacency mat and graph laplacian

---
+++ src/SpectralBisection.cpp
@@ -0,0 +1,49 @@
+#include<Eigen/Dense>
+
+using namespace std;
+using namespace Eigen;
+
+MatrixXd ComputeAdjacencyMatrix(MatrixX2d &edgeMat, int numNodes, int numEdges){ 
+
+	MatrixXd adjMat(numNodes,numNodes); 
+
+	// Adjacency mat gets 1 where there's a connection btwn 2 nodes
+	// 	may need to replace with weights ...
+	for( int i=0, i<numEdges, i++){
+		adjMat(edgeMat(i,1),edgeMat(i,2)) = 1;
+		adjMat(edgeMat(i,2),edgeMat(i,1)) = 1;
+	}
+
+	return adjMat;
+
+}
+
+MatrixXd ComputeGraphLaplacian(MatrixXd &adjMat, int matLen) {
+
+
+	MatrixXd L(matLen,matLen);
+	
+	// Sum along columns to get degrees
+	VectorXd partialSum = adjMat.colwise().sum();
+	
+	// Diagonal elements L = D-A
+	for( int i = 0, i<matLen, i++)
+		L(i,i) = partialSum(i) - adjMat(i,i);
+
+	// Off diagonals
+	for( i = 0, i<matLen, i++){
+	  for( int j = i+1, i<matLen,j++){
+		L(i,j) = -adjMat(i,j);
+		L(j,i) = -adjMat(j,i);
+	  }
+	}
+
+	return L;
+}
+
+
+VectorXd ComputeEigen2(MatrixXd &L) {
+
+
+
+}
---
+++ src/SpectralBisection.h
@@ -0,0 +1,26 @@
+#include<Eigen/Dense>
+
+
+/*   Calculate the adjacency matrix from the (coarsened)
+ * list of edges.
+ *
+ * Inputs
+ * edgeMat: M x 2 matrix containing list of edges
+ * numNodes: number of nodes in graph
+ * edges: number of edges in graph, number of rows in edgeMat
+ */
+MatrixXd ComputeAdjacencyMatrix(MatrixX2d &edgeMat, int numNodes, int numEdges)
+
+/*   Calculate the out degree matrix associated 
+ * with the (square) adjacency matrix.
+ *
+ * Inputs
+ * adjMat: adjacency matrix
+ * matLen: matrix length, adjMat is matLen x matLen
+ */
+MatrixXd ComputeDegreeMatrix(MatrixXd &adjMat, int matLen)
+
+
+
+
+
