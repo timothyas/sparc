88f487947c13bb1a22efb2da5ee0bbee884e9fb4
tsmith-stampede <timsmith204@utexas.edu>
2017-04-15 21:26:27 -0500

    Correctly compute adjacency matrix and graph laplacian

--- src/spectralBisection.cpp
+++ src/spectralBisection.cpp
@@ -8,12 +8,17 @@ using namespace Eigen;
 int computeAdjacencyMatrix(Graph* bisectMe, MatrixXd &adjMat)
 { 
 
+	int m,n;
+
 	// Adjacency mat gets 1 where there's a connection btwn 2 nodes
 	// 	may need to replace with weights ...
 	for( int i=0; i<bisectMe->getNumEdges(); i++)
 	{
-		adjMat(bisectMe->getEdgePoint(i,1),bisectMe->getEdgePoint(i,2)) = 1;
-		adjMat(bisectMe->getEdgePoint(i,2),bisectMe->getEdgePoint(i,1)) = 1;
+		m = (int)bisectMe->getEdgePoint(i,0);
+		n = (int)bisectMe->getEdgePoint(i,1);
+		
+		adjMat(m,n) = 1;
+		adjMat(n,m) = 1;
 	}
 
 	return 0;
@@ -32,7 +37,7 @@ int computeGraphLaplacian(MatrixXd &adjMat, MatrixXd &L, int matLen) {
 	// Off diagonals
 	for( int i = 0; i<matLen; i++)
 	{
-	  for( int j = i+1; i<matLen; j++)
+	  for( int j = i+1; j<matLen; j++)
 	  {
 		L(i,j) = -adjMat(i,j);
 		L(j,i) = -adjMat(j,i);
--- test/regression/banded_matrix.cpp
+++ test/regression/banded_matrix.cpp
@@ -15,18 +15,28 @@ int main()
 
 	// Compute adjacency A and graph laplacian L
 	int N = ptr->getNumNodes();
-	MatrixXd A(N,N);
-	MatrixXd L(N,N);
+	MatrixXd A = MatrixXd::Zero(N,N);
+	MatrixXd L = MatrixXd::Zero(N,N);
 
 	computeAdjacencyMatrix(ptr,A);
 	computeGraphLaplacian(A,L,N);
 
 	// Output for check
 	cout << "Adjacency matrix: " << endl;
-	cout << A << "\n --- \n " << endl;
+	cout << A(0,0) << " " << A(0,1) << " " << A(0,2) << " "<< A(0,3) << endl;
+	cout << A(1,0) << " " << A(1,1) << " " << A(1,2) << " " << A(1,3) << endl;
+	cout << A(2,0) << " " << A(2,1) << " " << A(2,2) << " "<< A(2,3) << endl;
+	cout << A(3,0) << " " << A(3,1) << " " << A(3,2) << " " << A(3,3) << endl;
+	cout << "\n --- \n " << endl;
 
-	cout << "Graph laplacian: " << endl;
-	cout << L << " \n --- \n " << endl;
+	cout << "Laplacian: " << endl;
+	cout << L(0,0) << " " << L(0,1) << " " << L(0,2) << " " << L(0,3)<< endl;
+	cout << L(1,0) << " " << L(1,1) << " " << L(1,2) << " " << L(1,3) << endl;
+	cout << L(2,0) << " " << L(2,1) << " " << L(2,2) << " " << L(2,3)<< endl;
+	cout << L(3,0) << " " << L(3,1) << " " << L(3,2) << " " << L(3,3) << endl;
+	cout << "\n --- \n " << endl;
+	// cout << "Graph laplacian: " << endl;
+	// cout << L << " \n --- \n " << endl;
 
 	return 0; 
 }
