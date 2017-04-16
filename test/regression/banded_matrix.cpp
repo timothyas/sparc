1b00cb678909c47c5f0cf91937439b26d0b67364
tsmith-stampede <timsmith204@utexas.edu>
2017-04-15 20:31:17 -0500

    check adjacency and laplacian

--- test/regression/banded_matrix.cpp
+++ test/regression/banded_matrix.cpp
@@ -2,6 +2,23 @@
 
 int main()
 {
-	Graph banded_matrix("banded_matrix.dat");
+	Graph* banded_matrix("banded_matrix.dat");
+
+
+	// Compute adjacency A and graph laplacian L
+	int N = banded_matrix->getNumNodes();
+	MatrixXd A(N,N);
+	MatrixXd L(N,N);
+
+	ComputeAdjacencyMatrix(banded_matrix,A);
+	ComputeGraphLaplacian(A,L,N);
+
+	// Output for check
+	cout << "Adjacency matrix: " << endl;
+	cout << A << "\n --- \n " << end;
+
+	cout << "Graph laplacian: " << endl;
+	cout << L << " \n --- \n " << endl;
+
 	return 0; 
 }
