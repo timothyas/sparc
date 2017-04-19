58eab5eaacf36eed9086a330d51a24f402d8e83d
timothys-mac <timsmith204@utexas.edu>
2017-04-19 10:13:25 -0500

    matlab file updated

--- data/readMat.m
+++ data/readMat.m
@@ -2,16 +2,23 @@ function [] = readMat()
 
 
 N = 4039;
-fname='Matrix.dat';
+fname1='matrix.dat';
+fname2='newMatrix.dat';
 
 
-fid=fopen(fname,'r');
-mat = fscanf(fid,'%d');
+
+fid=fopen(fname1,'r');
+mat1 = fscanf(fid,'%d');
+fclose(fid);
+fid=fopen(fname2,'r');
+mat2 = fscanf(fid,'%d');
 fclose(fid);
 
-mat=reshape(mat,[N N]);
+mat1=reshape(mat1,[N N]);
+mat2=reshape(mat2,[N N]);
 
-imagesc(mat(1:N/2,1:N/2))
+figure; spy(mat1)
+figure; spy(mat2)
 
 keyboard
 
