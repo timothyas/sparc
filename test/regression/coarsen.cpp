6b12a6d33e0bec2f8e251d1d2b4cd7e56165c4cc
timothys-mac <timsmith204@utexas.edu>
2017-04-23 17:33:45 -0500

    coarsening test initial commit, moving to ICES machine

---
+++ test/regression/coarsen.cpp
@@ -0,0 +1,47 @@
+#include<Eigen/Dense>
+#include"SymEigsSolver.h"
+#include<Eigen/Eigenvalues>
+#include<iostream>
+#include<iomanip>
+#include"graph.h"
+#include"coarsen.h"
+#include"spectralBisection.h"
+#include<math.h>
+#include<ctime>
+
+using namespace std;
+using namespace Eigen;
+
+
+int main()
+{
+	cout << "----------------------------------------------------------------------" << endl; 
+	cout << "Running Graph Coarsening Test" << endl; 
+	cout << "----------------------------------------------------------------------" << endl; 
+
+	Graph coarsen_me("coarsen.dat");
+	Graph* ptr = &coarsen_me;
+
+        cout << " --- Test matrix read, coloring ..." << endl;
+        
+        vector<int> colorList(coarsen_me.getNumNodes(), -1);
+	std::clock_t start;
+	double duration;
+
+	start=std::clock();
+
+        colorGraph_shared(coarsen_me, colorList);
+
+	duration = (std::clock() - start) / (double) CLOCKS_PER_SEC;
+        cout << " --- Coloring done, time: " << duration << endl;
+
+        cout << " --- Color list --- " << endl << endl;
+
+        for( int i=0; i<coarsen_me.getNumNodes(), i++)
+          cout << "Node: " << i << "    Color: " << colorList[i] << endl;
+
+        return 0;
+
+}
+        
+
