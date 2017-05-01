16c2268af01952c083e649ea78330c6e29477777
Gopal Yalla <gopal@ices.utexas.edu>
2017-05-01 11:57:49 -0500

    Updated main usage. Pass graph data on command line to avoid multiple compilation

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<iomanip>
 #include<math.h>
+#include"string.h"
 //#include"coarsen.h"
 #include"graph.h"
 #include"spectralBisection.h"
@@ -8,11 +9,25 @@
 
 using namespace std;
 
-
-int main()
+void usage()
 {
+	cout << "usage: ./spiC [-h] filename" << endl << endl;
+	cout << "positional arguments:" << endl; 
+	cout << "\tfilename\tfile containing graph data. Note, file should have number of nodes and number of edges at the top of the file." << endl; 
+	cout << "optional arguments:" << endl;
+	cout << "\t-h,--help\tshow this help message and exit." << endl; 
+}
 
-	Graph G("../data/facebook_combined.txt");
+int main(int argc, char * argv[])
+{
+	if (argc == 1 || strcmp(argv[1],"-h")==0 || strcmp(argv[1],"--help")==0)
+	{
+		usage();
+		return 0;
+	}
+
+	string input_file = argv[1]; 
+	Graph G(input_file);
 	Graph *ptr = &G; 
 	spectralBisection(ptr);
 	return 0;
