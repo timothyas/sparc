e782b9e9bd018d9285438edd3ea592dca087304a
timothys-thorne <timsmith204@utexas.edu>
2017-05-03 23:57:36 -0500

    muted coarsen test output

--- test/regression/coarsen_test.cpp
+++ test/regression/coarsen_test.cpp
@@ -15,9 +15,7 @@ int runCoarsenTest()
 	cout << "----------------------------------------------------------------------" << endl; 
 
 	Graph coarsen_me("coarsen_test.dat");
-	//Graph* ptr = &coarsen_me;
 
-        //int numColors;
         vector<int> colorList(coarsen_me.getNumNodes(), -1);
 	std::clock_t start;
 	double duration;
@@ -48,22 +46,21 @@ int runCoarsenTest()
         }
 
         cout << "Successfully colored graph in " << duration << " sec" << endl;
+*/        
 
-        // --- Maximal matching test
-
+        // --- Create coarsened graph
         start=std::clock();
-        mxm_shared(coarsen_me, colorList, numColors); 
+        Graph parent;
+	parent.coarsenFrom(coarsen_me); 
         duration=(std::clock()-start) / (double) CLOCKS_PER_SEC;
 
+        // --- Maximal matching test
         for( int i=0; i<coarsen_me.getNumNodes(); i++){
           for ( int j=i+1; j<coarsen_me.getNumNodes(); j++){
             if( coarsen_me.getNodeMatch(i) == coarsen_me.getNodeMatch(j) && coarsen_me.getNodeMatch(i) != -1 ){
               cout << "MATCHING ERROR: Found multiple node matching. " << endl;
               cout << " -- Nodes: " << i << " and " << j << " are trying to match " << coarsen_me.getNodeMatch(i) << endl;
             
-
-              cout << " Matching done, time: " << duration << endl;
-
               cout << " --- Match list --- " << endl;
               for( int i=0; i<coarsen_me.getNumNodes(); i++){
                 cout << coarsen_me.getNodeMatch(i) << endl;
@@ -75,51 +72,44 @@ int runCoarsenTest()
 
         cout << "Successfully computed maximal matching in " << duration << " sec" << endl;
 
-        // --- Create coarsened graph
-*/        
-        start=std::clock();
-        Graph parent;
-	parent.coarsenFrom(coarsen_me); 
-        duration=(std::clock()-start) / (double) CLOCKS_PER_SEC;
-
-        cout << " --- Child Match list --- " << endl;
-        for( int i=0; i<coarsen_me.getNumNodes(); i++){
-          cout << coarsen_me.getNodeMatch(i) << endl;
-        }
-        cout << " --- Coarse neighbor list --- " << endl;
-        for( int i =0; i<parent.getNumNodes(); i++){
-          cout << "node " << i << " : ";
-          for(unsigned int j =0; j<parent.getNeighbors(i).size(); j++){
-            cout << parent.getNeighbors(i)[j] << " ";
-          }
-          cout << endl;
-        }
-        cout << " --- Parent list --- " << endl;
-        for( int i=0; i<parent.getNumNodes(); i++){
-          cout << "parent node " << i << " has children :  ";
-          for (unsigned int j =0; j< parent.getChildren(i).size(); j++){
-            cout << parent.getChildren(i)[j] << " ";
-          }
-          cout << endl;
-        }
-        cout << " --- Coarsened edge weight --- " << endl;
-        for( int i =0; i<parent.getNumNodes(); i++){
-          cout << "node " << i << " : ";
-          for(unsigned int j =0; j<parent.getEdges(i).size(); j++){
-            cout << parent.getEdges(i)[j] << " ";
-          }
-          cout << endl;
-        }
-
-        cout << " --- Coarsened node weight --- " << endl;
-        for( int i =0; i<parent.getNumNodes(); i++){
-          cout << "node " << i << " : " << parent.getNodeWeight(i) << endl;
-        }
-
-        cout << " --- Coarsened edge list --- " << endl;
-        for( int i =0; i<parent.getNumEdges(); i++){
-          cout << parent.getEdgePoint(i,0) << " " << parent.getEdgePoint(i,1) << endl;
-        }
+//        cout << " --- Child Match list --- " << endl;
+//        for( int i=0; i<coarsen_me.getNumNodes(); i++){
+//          cout << coarsen_me.getNodeMatch(i) << endl;
+//        }
+//        cout << " --- Coarse neighbor list --- " << endl;
+//        for( int i =0; i<parent.getNumNodes(); i++){
+//          cout << "node " << i << " : ";
+//          for(unsigned int j =0; j<parent.getNeighbors(i).size(); j++){
+//            cout << parent.getNeighbors(i)[j] << " ";
+//          }
+//          cout << endl;
+//        }
+//        cout << " --- Parent list --- " << endl;
+//        for( int i=0; i<parent.getNumNodes(); i++){
+//          cout << "parent node " << i << " has children :  ";
+//          for (unsigned int j =0; j< parent.getChildren(i).size(); j++){
+//            cout << parent.getChildren(i)[j] << " ";
+//          }
+//          cout << endl;
+//        }
+//        cout << " --- Coarsened edge weight --- " << endl;
+//        for( int i =0; i<parent.getNumNodes(); i++){
+//          cout << "node " << i << " : ";
+//          for(unsigned int j =0; j<parent.getEdges(i).size(); j++){
+//            cout << parent.getEdges(i)[j] << " ";
+//          }
+//          cout << endl;
+//        }
+//
+//        cout << " --- Coarsened node weight --- " << endl;
+//        for( int i =0; i<parent.getNumNodes(); i++){
+//          cout << "node " << i << " : " << parent.getNodeWeight(i) << endl;
+//        }
+//
+//        cout << " --- Coarsened edge list --- " << endl;
+//        for( int i =0; i<parent.getNumEdges(); i++){
+//          cout << parent.getEdgePoint(i,0) << " " << parent.getEdgePoint(i,1) << endl;
+//        }
 
 
         cout << "Created coarse graph in " << duration << " sec" << endl;
