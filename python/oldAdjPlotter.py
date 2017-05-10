44c6a25fcc2f69d01ca7dc24c07867598ba83925
timothys-thorne <timsmith204@utexas.edu>
2017-05-10 11:49:05 -0500

    plots done

--- python/oldAdjPlotter.py
+++ python/oldAdjPlotter.py
@@ -8,20 +8,21 @@ import matplotlib.pyplot as plt
 import scipy.sparse as sps
 
 
-def adj_plotter(readname,writename)
-    # Read results and fill containers
-    a=np.loadtxt(readname)
-    a=csc_matrix(a)
-    
-
-    plt.figure(figsize=(15,15))
-    mh=plt.spy(a,markersize=1.5)
-    plt.spy(a.transpose(),markersize=1.5)
+def adj_plotter(readname,writename):
+        # Read results and fill containers
+        a=np.loadtxt(readname)
+        a=sps.csc_matrix(a)
+        
+        
+        plt.figure(figsize=(15,15))
+        mh=plt.spy(a,markersize=1.5)
+        plt.spy(a.transpose(),markersize=1.5)
+        plt.savefig(writename,bbox_inches='tight',dpi=100)
 
 
 # --- Directory
 oldResults="../results/multi-coarsen-12-procs/old"
-oldFigs="../results/multi-coarsen-12-procs/old"
+oldFigs="../figs/multi-coarsen-12-procs/old"
 
 adj_plotter( ("%s/adjacency_02c.dat" % oldResults), ("%s/adjacency_02c.png" % oldFigs) )
 adj_plotter( ("%s/adjacency_09c.dat" % oldResults), ("%s/adjacency_09c.png" % oldFigs) )
